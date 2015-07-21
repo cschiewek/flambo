f50c8fb7870946ce328c675acf47c67a9c4830d6
Curtis Schiewek <curtis.schiewek@gmail.com>
2015-07-20 21:26:29 -0400

    NOW WITH TESTS! 👉👊

--- config/test.exs
+++ config/test.exs
@@ -4,6 +4,7 @@ use Mix.Config
 # you can enable the server option below.
 config :flambo, Flambo.Endpoint,
   http: [port: 4001],
+  token: "token",
   server: false
 
 # Print only warnings and errors during test
@@ -12,6 +13,7 @@ config :logger, level: :warn
 # Configure your database
 config :flambo, Flambo.Repo,
   adapter: Ecto.Adapters.Postgres,
+  pool: Ecto.Adapters.SQL.Sandbox,
   username: "postgres",
   password: "postgres",
   database: "flambo_test",
---
+++ test/controllers/slack_controller_test.exs
@@ -0,0 +1,44 @@
+defmodule Flambo.SlackControllerTest do
+  use Flambo.ConnCase
+  @base_params [token: "token", trigger_word: "flambo", user_name: "Finn"]
+
+  test "GET /" do
+    conn = get conn(), "/"
+    assert response(conn, 404)
+  end
+
+  test "POST / with invalid token" do
+    conn = post conn(), "/", [token: ""]
+    assert response(conn, 403)
+  end
+
+  test "POST / with unknown command" do
+    conn = post conn(), "/", @base_params ++ [text: "flambo mathmatical"]
+    assert conn.resp_body =~ "Sorry Finn! I don't know how to do that."
+  end
+
+  test "POST / with hi" do
+    conn = post conn(), "/", @base_params ++ [text: "flambo hi"]
+    assert conn.resp_body =~ "Why hello there Finn!"
+  end
+
+  test "POST / with help" do
+    conn = post conn(), "/", @base_params ++ [text: "flambo help"]
+    assert response(conn, 200)
+  end
+
+  test "POST / with who are you" do
+    conn = post conn(), "/", @base_params ++ [text: "flambo who are you"]
+    assert response(conn, 200)
+  end
+
+  test "POST / with tell" do
+    conn = post conn(), "/", @base_params ++ [text: "flambo tell @iceking not cool"]
+    assert conn.resp_body =~ "@iceking not cool"
+  end
+
+  test "POST / with gif" do
+    conn = post conn(), "/", @base_params ++ [text: "flambo gif princess bubblegum"]
+    assert response(conn, 200)
+  end
+end
