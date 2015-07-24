2e05f63fb95fb7754745e5662c7b9f0261648f79
Curtis Schiewek <curtis.schiewek@gmail.com>
2015-07-24 18:01:46 -0400

    setup relisa, again

--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule Flambo.Mixfile do
      {:phoenix_html, "~> 1.0"},
      {:phoenix_live_reload, "~> 0.4", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:httpoison, "~> 0.7.0"}]
+     {:httpoison, "~> 0.7.0"},
+     {:relisa, "~> 0.1.0"}]
   end
 end
