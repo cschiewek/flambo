e314b7ad1e89b989ed088ca9b63008d6d078a2c7
Curtis Schiewek <curtis.schiewek@gmail.com>
2015-07-23 22:37:12 -0400

    removed local secret sample and moved prod host and port to secret

--- config/local.secret.sample.exs
+++
@@ -1,7 +0,0 @@
-use Mix.Config
-
-config :flambo, Flambo.Endpoint,
-  token: "", # Slack webhook token
-  giphy_key: "dc6zaTOxFJmzC",
-  google_key: "", # Google Custom Search Key
-  google_search_id: "", # Goggle Custom Search ID
