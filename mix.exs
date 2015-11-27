ee0b80270c43e2e8ed42154216e365fa4840b116
Curtis Schiewek <curtis.schiewek@gmail.com>
2015-11-27 12:34:03 -0500

    update mandrag

--- mix.exs
+++ mix.exs
@@ -36,6 +36,6 @@ defmodule Flambo.Mixfile do
      {:phoenix_live_reload, "~> 1.0.1", only: :dev},
      {:cowboy, "~> 1.0.4"},
      {:httpoison, "~> 0.8.0"},
-     {:mandrag, "~> 0.0.1"}]
+     {:mandrag, "~> 0.0.2"}]
   end
 end
