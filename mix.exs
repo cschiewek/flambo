ba7d1b36478eb21e4508042066d7b46ab7fd53ac
Curtis Schiewek <curtis.schiewek@gmail.com>
2015-11-27 11:50:05 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Flambo.Mixfile do
 
   def project do
     [app: :flambo,
-     version: "0.3.3",
+     version: "0.3.4",
      elixir: "~> 1.1",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
