31932e0ca81757df1f635958d8c298dd71c1b2d7
Curtis Schiewek <curtis.schiewek@gmail.com>
2016-02-06 00:40:18 -0500

    remove postgrex

--- mix.exs
+++ mix.exs
@@ -29,7 +29,6 @@ defmodule Flambo.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [{:phoenix, "~> 1.1.4"},
-     {:postgrex, "~> 0.11.0"},
      {:phoenix_html, "~> 2.5.0"},
      {:phoenix_live_reload, "~> 1.0.3", only: :dev},
      {:cowboy, "~> 1.0.4"},
