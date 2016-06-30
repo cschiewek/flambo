47d20d5118e4756354561425c4ecc9966f9b53e7
Curtis Schiewek <curtis.schiewek@gmail.com>
2016-06-30 15:09:03 -0400

    update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Flambo.Mixfile do
 
   def project do
     [app: :flambo,
-     version: "0.1.0",
+     version: "0.4.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
