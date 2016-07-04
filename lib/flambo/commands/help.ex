a482b1dec65f26a7a5d6bfa0e2714ef312ddd7cd
Curtis Schiewek <curtis.schiewek@gmail.com>
2016-07-04 16:59:15 -0400

    fix help syntax

--- lib/flambo/commands/help.ex
+++ lib/flambo/commands/help.ex
@@ -8,18 +8,18 @@ defmodule Flambo.Commands.Help do
     ```
     Flambo
     =========================================================================
-    #{trigger} animate        display a random gif
-    #{trigger} bomb <n>       displays <n> random images (default for n is 3)
-    #{trigger} gif            display a random gif
-    #{trigger} help           prints this help message
-    #{trigger} hi             says hi
-    #{trigger} image          shows a random image
-    #{trigger} say            says something
-    #{trigger} show           shows a random image
-    #{trigger} tell           tell someone something
-    #{trigger} watch          displays a youtube video
-    #{trigger} who are you?   tells you who he is
-    #{trigger} youtube        displays a youtube video
+    @#{trigger} animate        display a random gif
+    @#{trigger} bomb <n>       displays <n> random images (default for n is 3)
+    @#{trigger} gif            display a random gif
+    @#{trigger} help           prints this help message
+    @#{trigger} hi             says hi
+    @#{trigger} image          shows a random image
+    @#{trigger} say            says something
+    @#{trigger} show           shows a random image
+    @#{trigger} tell           tell someone something
+    @#{trigger} watch          displays a youtube video
+    @#{trigger} who are you?   tells you who he is
+    @#{trigger} youtube        displays a youtube video
     ==========================================================================
     Want more commands? Fork it! https://github.com/cschiewek/flambo
     ```
